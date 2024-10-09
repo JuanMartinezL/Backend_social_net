8017b0ddbd4ee1fe078f2202ab5987bb28c02316
Juan Martinez <juanmart600@gmail.com>
2024-10-08 20:24:40 -0500

    configure index.js

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+import { connect } from "mongoose";
+import dotenv from "dotenv";
+
+// Configurar el dotenv para usar variables de entorno
+dotenv.config();
+
+const connection = async() => {
+  try {
+    await connect(process.env.MONGODB_URI);
+    console.log("Te has conectado correctamente a DB_Social_Network 🥳");
+  } catch (error) {
+    console.log("Error al conectar la BD 😯", error);
+    throw new Error("¡No se ha podido conectar a la base de datos! 😒");
+  }
+};
+
+export default connection;
